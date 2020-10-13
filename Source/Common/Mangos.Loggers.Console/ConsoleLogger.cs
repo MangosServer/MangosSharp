3dbefdcb51476b538d8e12448b36b550ed5175d7
Romfos <Romfos@tut.by>
2020-10-13 19:51:51 +0300

    Added Mangos.Loggers subsystem

---
+++ Source/Common/Mangos.Loggers.Console/ConsoleLogger.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Mangos.Loggers.Console
+{
+	public class ConsoleLogger : ILogger
+	{
+		private static readonly object _lockObject = new object();
+
+		public void Debug(string format, params object[] args)
+		{
+			Write(ConsoleColor.Gray, format, args);
+		}
+
+		public void Message(string format, params object[] args)
+		{
+			Write(ConsoleColor.White, format, args);
+		}
+
+		public void Warning(string format, params object[] args)
+		{
+			Write(ConsoleColor.Yellow, format, args);
+		}
+
+		public void Error(string format, params object[] args)
+		{
+			Write(ConsoleColor.Red, format, args);
+		}
+
+		private void Write(ConsoleColor color, string format, object[] args)
+		{
+			lock (_lockObject)
+			{
+				System.Console.ForegroundColor = color;
+				System.Console.WriteLine(Format(format, args));
+			}
+		}
+
+		private string Format(string format, object[] args)
+		{
+			return string.Format("[{0}] {1}", DateTime.Now, string.Format(format, args));
+		}
+	}
+}
---
+++ Source/Common/Mangos.Loggers/ILogger.cs
@@ -0,0 +1,13 @@
+﻿namespace Mangos.Loggers
+{
+	public interface ILogger
+    {
+		void Debug(string format, params object[] args);
+
+		void Message(string format, params object[] args);
+
+		void Warning(string format, params object[] args);
+
+		void Error(string format, params object[] args);
+	}
+}
