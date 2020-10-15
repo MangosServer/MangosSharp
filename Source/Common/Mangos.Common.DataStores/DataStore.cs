af22dd2309d5793f6a8ec1f740fc4658155d127e
Romfos <Romfos@tut.by>
2020-10-15 22:07:47 +0300

    Added DataStoreProvider

---
+++ Source/Common/Mangos.Common.DataStores/DataStore.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mangos.Common.DataStores
+{
+    public class DataStore
+    {
+        private byte[] data;
+
+        [Description("Header information: File type.")]
+        public string Type;
+
+        [Description("Header information: Rows contained in the file.")]
+        public int Rows;
+
+        [Description("Header information: Columns for each row.")]
+        public int Columns;
+
+        [Description("Header information: Bytes ocupied by each row.")]
+        public int RowLength;
+
+        [Description("Header information: Strings data block length.")]
+        public int StringBlockLength;
+
+        public void LoadFromFile(string path)
+        {
+            data = File.ReadAllBytes(path);
+
+            Type = Encoding.ASCII.GetString(data, 0, 4);
+            Rows = BitConverter.ToInt32(new ReadOnlySpan<byte>(data, 4, 4));
+            Columns = BitConverter.ToInt32(new ReadOnlySpan<byte>(data, 8, 4));
+            RowLength = BitConverter.ToInt32(new ReadOnlySpan<byte>(data, 12, 4));
+            StringBlockLength = BitConverter.ToInt32(new ReadOnlySpan<byte>(data, 16, 4));
+        }
+
+        public async Task LoadFromFileAsync(string path)
+        {
+            data = await File.ReadAllBytesAsync(path);
+
+            Type = Encoding.ASCII.GetString(data, 0, 4);
+            Rows = BitConverter.ToInt32(new ReadOnlySpan<byte>(data, 4, 4));
+            Columns = BitConverter.ToInt32(new ReadOnlySpan<byte>(data, 8, 4));
+            RowLength = BitConverter.ToInt32(new ReadOnlySpan<byte>(data, 12, 4));
+            StringBlockLength = BitConverter.ToInt32(new ReadOnlySpan<byte>(data, 16, 4));
+        }
+
+        public int ReadInt(int row, int column)
+        {
+            return BitConverter.ToInt32(data, GetOffset(row, column));
+        }
+
+        public float ReadFloat(int row, int column)
+        {
+            return BitConverter.ToSingle(data, GetOffset(row, column));
+        }
+
+        public string ReadString(int row, int column)
+        {
+            var offset = GetRowOffset(row) + ReadInt(row, column);
+            var length = Array.IndexOf<byte>(data, 0, offset) - offset;
+            return BitConverter.ToString(data, offset, length);
+        }
+
+        private int GetOffset(int row, int column)
+        {
+            if (column >= Columns)
+            {
+                throw new ApplicationException("DBC: Column index outside file definition.");
+            }
+            return GetRowOffset(row) + column * 4;
+        }
+
+        private int GetRowOffset(int row)
+        {
+            if (row >= Rows)
+            {
+                throw new ApplicationException("DBC: Row index outside file definition.");
+            }
+
+            return 20 + row * RowLength;
+        }
+    }
+}
---
+++ Source/Common/Mangos.Common.DataStores/DataStoreProvider.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Mangos.Common.DataStores
+{
+    public class DataStoreProvider
+    {
+        private readonly string dbcDirectory = "dbc";
+
+        private Dictionary<string, DataStore> dataStores;
+
+        public DataStoreProvider()
+        {
+            dataStores = new Dictionary<string, DataStore>();
+        }
+
+        public DataStore GetDataStore(string dbcFileName)
+        {
+            if (dataStores.ContainsKey(dbcFileName))
+            {
+                return dataStores[dbcFileName];
+            }
+            else
+            {
+                var path = Path.Combine(dbcDirectory, dbcFileName);
+                var dataStore = new DataStore();
+                dataStore.LoadFromFile(path);
+                dataStores[dbcFileName] = dataStore;
+                return dataStore;
+            }
+        }
+
+        public async ValueTask<DataStore> GetDataStoreAsync(string dbcFileName)
+        {
+            if(dataStores.ContainsKey(dbcFileName))
+            {
+                return dataStores[dbcFileName];
+            }
+            else
+            {
+                var path = Path.Combine(dbcDirectory, dbcFileName);
+                var dataStore = new DataStore();
+                await dataStore.LoadFromFileAsync(path);
+                dataStores[dbcFileName] = dataStore;
+                return dataStore;
+            }
+        }
+    }
+}
