d85dd29cb8c085e3f4c2f3a649e11283c2aa1310
Meltie <mfrink17@gmail.com>
2021-03-05 13:43:22 -0600

    Testing whitespace detect script * Making sure the script can detect the whitespaces by using a random file.

--- Source/Common/Mangos.Common/Globals/EUnitFields.cs
+++ Source/Common/Mangos.Common/Globals/EUnitFields.cs
@@ -15,7 +15,7 @@
 //  along with this program. If not, write to the Free Software
 //  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //
-
+	 	
 namespace Mangos.Common.Globals
 {
     public enum EUnitFields
