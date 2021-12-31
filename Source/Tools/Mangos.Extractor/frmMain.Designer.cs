f8714a84572817b61e2e27e09d8ae5f1377ba19e
Romfos <Romfos@tut.by>
2021-12-31 20:14:35 +0300

    Update Mangos.Extractor

--- Source/Tools/Mangos.Extractor/frmMain.Designer.cs
+++ Source/Tools/Mangos.Extractor/frmMain.Designer.cs
@@ -11,27 +11,6 @@ namespace Mangos.Extractor
     [DesignerGenerated()]
     public partial class frmMain : Form
     {
-
-        // Form overrides dispose to clean up the component list.
-        [DebuggerNonUserCode()]
-        protected override void Dispose(bool disposing)
-        {
-            try
-            {
-                if (disposing && components is object)
-                {
-                    components.Dispose();
-                }
-            }
-            finally
-            {
-                base.Dispose(disposing);
-            }
-        }
-
-        // Required by the Windows Form Designer
-        private IContainer components;
-
         // NOTE: The following procedure is required by the Windows Form Designer
         // It can be modified using the Windows Form Designer.  
         // Do not modify it using the code editor.
